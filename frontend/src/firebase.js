28203f5bc3a96769cc447dd145f1dab7294dac28
Dhruv Kumar Sharma <dhruv.kccsw@kccitm.edu.in>
2026-02-02 15:51:27 +0530

    fixed api key public

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjQyJkX9K0IATW7hnIJ-CXiWhMUsQWOLM",
-  authDomain: "career-library.firebaseapp.com",
-  projectId: "career-library",
-  storageBucket: "career-library.firebasestorage.app",
-  messagingSenderId: "461868870638",
-  appId: "1:461868870638:web:64a6d561f1a264db22bcff"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
 };
 
 // Initialize Firebase
